9ce6832989c38a81fde6055ab0ca471eba9b9829
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2024-04-03 00:31:01 -0300

    Gerando Spring Initializr

---
+++ src/main/java/projeto/pi/reciclemultiplique/ReciclemultipliqueApplication.java
@@ -0,0 +1,13 @@
+package projeto.pi.reciclemultiplique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclemultipliqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclemultipliqueApplication.class, args);
+	}
+
+}
