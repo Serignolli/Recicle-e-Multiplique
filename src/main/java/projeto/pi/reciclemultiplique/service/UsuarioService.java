77ede037fb5da588b2eaa5f6194d0afd3b35b51d
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2024-05-02 20:20:10 -0300

    Adicionando camada de serviço (Usuario/Empresa)

---
+++ src/main/java/projeto/pi/reciclemultiplique/service/UsuarioService.java
@@ -0,0 +1,18 @@
+package projeto.pi.reciclemultiplique.service;
+
+import java.util.List;
+
+import projeto.pi.reciclemultiplique.entity.Usuario;
+
+public interface UsuarioService {
+	
+	void salvar(Usuario usuario);
+	
+	void editar(Usuario usuario);
+	
+	void excluir(Long id);
+	
+	Usuario buscarPorId(Long id);
+	
+	List<Usuario> buscarTodos();
+}
