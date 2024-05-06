f0e61959aae28af1987e3c50ed60198915c32e5b
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2024-05-06 20:37:10 -0300

    Configurando cadastro de usuário

--- src/main/java/projeto/pi/reciclemultiplique/controller/UsuarioController.java
+++ src/main/java/projeto/pi/reciclemultiplique/controller/UsuarioController.java
@@ -1,20 +1,28 @@
 package projeto.pi.reciclemultiplique.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import projeto.pi.reciclemultiplique.entity.Usuario;
+import projeto.pi.reciclemultiplique.service.UsuarioService;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @PostMapping("/cadastrar")
-    @ResponseBody
-    public String cadastrar(@RequestBody Usuario usuario) {
-        return "Usuário cadastrado com sucesso!";
+    public String cadastrar(@Validated @RequestBody Usuario usuario, RedirectAttributes redirectAttributes) {
+        usuarioService.salvar(usuario);
+        redirectAttributes.addFlashAttribute("mensagem", "Usuário cadastrado com sucesso!");
+        return "redirect:/pagina_de_cadastro";
     }
+
 }
