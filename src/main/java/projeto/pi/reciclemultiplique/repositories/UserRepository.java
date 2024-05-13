0a17714bbdd3fac957ca4311b0bcb02b1c041ca0
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2024-05-12 23:58:33 -0300

    UserRepository

--- src/main/java/projeto/pi/reciclemultiplique/repositories/UserRepository.java
+++ src/main/java/projeto/pi/reciclemultiplique/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package projeto.pi.reciclemultiplique.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import projeto.pi.reciclemultiplique.domain.Usuario;
 
 public interface UserRepository extends JpaRepository<Usuario, Long>{
 
+	Optional<Usuario> findByEmail(String email);
 }
